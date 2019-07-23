9bfac68e58e72fd4ca2dfe4f322773e61a8ef4dd
Zachary Zhou <avengyy@gmail.com>
2019-07-23 12:54:18 +0800

    remove unused code

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,4 @@
 const expect = require('chai').expect;
-const assert = require('assert');
 const { multiply, divide } = require('../index');
 
 describe('Test Example 1', function() {
@@ -23,44 +22,3 @@ describe('Test Example 1', function() {
     });
   });
 });
-
-// describe('String#split', function() {
-//   it('should return an array', function() {
-//     assert(Array.isArray('a,b,c'.split(',')));
-//   });
-
-//   it('should return the same array', function() {
-//     assert.equal(
-//       ['a', 'b', 'c'].length,
-//       'a,b,c'.split(',').length,
-//       'arrays have equal length'
-//     );
-//     for (var i = 0; i < ['a', 'b', 'c'].length; i++) {
-//       assert.equal(
-//         ['a', 'b', 'c'][i],
-//         'a,b,c'.split(',')[i],
-//         i + 'element is equal'
-//       );
-//     }
-//   });
-// });
-
-// before(function() {
-//   expected = ['a', 'b', 'c'];
-// });
-
-// describe('New String#split', function() {
-//   beforeEach(function() {
-//     current = 'a,b,c'.split(',');
-//   });
-//   it('should return an array', function() {
-//     assert(Array.isArray(current));
-//   });
-//   it('should return the same array', function() {
-//     assert.equal(expected.length, current.length, 'arrays have equal length');
-
-//     for (let i = 0; i < expected.length; i++) {
-//       assert.equal(expected[i], current[i], i + 'element is equal');
-//     }
-//   });
-// });
